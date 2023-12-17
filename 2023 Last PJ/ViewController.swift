f6718984e21434007fd929c55da479d81826e4de
Geon_27 <yukgeon98527@gmail.com>
2023-12-17 09:51:10 +0900

    Initial Commit

---
+++ 2023 Last PJ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2023 Last PJ
+//
+//  Created by 육건 on 12/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
